72d5013ca77f796490f977570be44f0af3f81774
Matheus <zTeussandi@hotmail.com>
2021-12-20 21:34:55 -0300

    Update styles.ts

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -1,41 +1,43 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    margin-top: 4rem;
-    
+  margin-top: 4rem;
 
-    table {
-        width: 100%;
-        border-spacing: 0 0.5rem;
-    }
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
 
     th {
-        color: var(--text-body);
-        font-weight: 400;
-        padding: 1rem 2rem;
-        text-align: left;
-        line-height: 1.5rem;
+      color: var(--text);
+      font-weight: 400;
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
     }
 
     td {
-        padding: 1rem 2rem;
-        border: 0;
-        background: var(--shape);
-        color: var(--text-body);
-        border-radius: 0.25rem;
+      padding: 1rem 2rem;
+      border: 0;
+      background: var(--white);
+      color: var(--text);
+      border-radius: 0.25rem;
 
-        &.title {
-            color: var(--text-tile);
-        } 
-        
-        &.positive {
+      &:first-child {
+        color: var(--title);
+      }
+
+      &.deposit {
         color: var(--green);
-        }
+      }
 
-        &.negative {
+      &.withdraw {
         color: var(--red);
-        }
+      }
 
     }
+
+
+
+  }
 
 `
